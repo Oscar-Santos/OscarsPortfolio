8ba91945e015c025d2ac6246e73c2edf6cad6351
Oscar Santos <oscar303data@gmail.com>
2023-09-16 12:32:42 -0600

    add 2 more projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,5 @@
-
-
 import React from 'react';
 import { data } from "../data/data.js";
-
 
 const Projects = () => {
 
--- src/data/data.js
+++ src/data/data.js
@@ -1,39 +1,52 @@
-// import WorkIm from '../assets/projects/workImg.jpeg'
-// import RealEst from '../assets/projects/realestate.jpg'
 import Chatbot from '../assets/projects/chatbot.png'
 import TaskTracker from '../assets/projects/task_tracker.png'
 import HistoryMaps from '../assets/projects/history_maps.png'
 import WeatherSweather from '../assets/projects/weather_sweather.png'
+import Battleship from '../assets/projects/battleship.png'
+import Tic from '../assets/projects/tic.png'
 
 export const data=[
     {
         id:1,
-        name:"Virtual OpenAI Chatbot",
+        name:"Virtual OpenAI Chatbot App",
         image:Chatbot,
         github:"https://github.com/Oscar-Santos/Virtual_OpenAI_Chatbot",
         live:"https://virtual-open-ai-chatbot-k4odgur73-oscar-santos.vercel.app/",
     },
     {
         id:2,
+        name:"Tic Tac Toe Game",
+        image:Tic,
+        github:"https://github.com/Oscar-Santos/tic_tac_toe_game_javascript",
+        live:"https://oscar-santos.github.io/tic_tac_toe_game_javascript/",
+    },
+    {
+        id:3,
         name:"Task Tracker App",
         image:TaskTracker,
         github:"https://github.com/Oscar-Santos/TASK_TRACKER_App",
         live:"",
     },
     {
-        id:3,
-        name:"History Maps",
+        id:4,
+        name:"History Maps App",
         image:HistoryMaps,
         github:"https://github.com/History-Maps-2203",
         live:"",
     },
     {
-        id:4,
-        name:"Weather Sweather",
+        id:5,
+        name:"Weather Sweather App",
         image:WeatherSweather,
         github:"https://github.com/Oscar-Santos/weather_sweater",
         live:"",
     },
-
+    {
+        id:6,
+        name:"Batlleship Game",
+        image:Battleship,
+        github:"https://github.com/Oscar-Santos/battleship",
+        live:"",
+    }
 
 ]
