63d6209ff78da514b379b98db53cda22b229398e
Oscar Santos <oscar303data@gmail.com>
2023-09-16 08:38:08 -0600

    connect contact form with getform.io

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/00f63e9f-1505-4c0f-b389-97c0e6464773" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>
                    Contact Information
