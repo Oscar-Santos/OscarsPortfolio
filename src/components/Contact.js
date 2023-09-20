705d5f787e5797b879272cf77e9329e3fb36a15f
Oscar Santos <oscar303data@gmail.com>
2023-09-19 22:36:14 -0600

    fixing columns on placeholder for the Contact Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,17 @@ import Email from '../assets/gmail.png'
 
 const Contact = () => {
   return (
+    
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+        
+        
         <form method='POST' action="https://getform.io/f/00f63e9f-1505-4c0f-b389-97c0e6464773" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
                    Contact Information
                 </p>
 
@@ -39,7 +46,7 @@ const Contact = () => {
             <br />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
             <br />
-            <textarea className='bg-[#ccd6f6] p-2' placeholder='Message' name="Message" id="" cols="30" rows="10">
+            <textarea className='bg-[#ccd6f6] p-2' placeholder='Message' name="Message" id="" cols="30" rows="8">
             </textarea>
             <br />
             <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-6 py-4 my-8 mx-auto flex items-center'>
@@ -47,6 +54,7 @@ const Contact = () => {
             </button>
 
         </form>
+      
     </div>
   )
 }
