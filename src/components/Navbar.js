849790ebe1f7d2e1fff1a126266bd8e6b79e6e08
Oscar Santos <oscar303data@gmail.com>
2023-09-22 07:09:34 -0600

    add hover on name of the pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,31 +24,31 @@ const Navbar = () => {
         {/* menu */}
        
           <ul className='hidden md:flex text-2xl'>
-            <li>
+            <li className='hover:text-[#f6a35f]'>
               <Link to="home" smooth={true} duration={500} >
                 Home
               </Link>
             </li>
 
-            <li>
+            <li className='hover:text-[#f6a35f]'>
               <Link to="about" smooth={true} duration={500} >
                 About
               </Link>
             </li>
 
-            <li>
+            <li className='hover:text-[#f6a35f]'>
               <Link to="skills" smooth={true} duration={500} >
                 Skills
               </Link>
             </li>
 
-            <li>
+            <li className='hover:text-[#f6a35f]'>
               <Link to='projects' smooth={true} duration={500}>
                 Projects
               </Link>
             </li>
 
-            <li>
+            <li className='hover:text-[#f6a35f]'>
               <Link to="contact" smooth={true} duration={500} >
                 Contact
               </Link>
