0435591fb6cfb13e585306155b55f71eb956ded5
Oscar Santos <oscar303data@gmail.com>
2023-09-26 11:26:02 -0600

    refactor color on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,31 +24,31 @@ const Navbar = () => {
         {/* menu */}
        
           <ul className='hidden md:flex text-2xl'>
-            <li className='hover:text-[#f6a35f]'>
+            <li className='hover:text-[#f78d42]'>
               <Link to="home" smooth={true} duration={500} >
                 Home
               </Link>
             </li>
 
-            <li className='hover:text-[#f6a35f]'>
+            <li className='hover:text-[#f78d42]'>
               <Link to="about" smooth={true} duration={500} >
                 About
               </Link>
             </li>
 
-            <li className='hover:text-[#f6a35f]'>
+            <li className='hover:text-[#f78d42]'>
               <Link to="skills" smooth={true} duration={500} >
                 Skills
               </Link>
             </li>
 
-            <li className='hover:text-[#f6a35f]'>
+            <li className='hover:text-[#f78d42]'>
               <Link to='projects' smooth={true} duration={500}>
                 Projects
               </Link>
             </li>
 
-            <li className='hover:text-[#f6a35f]'>
+            <li className='hover:text-[#f78d42]'>
               <Link to="contact" smooth={true} duration={500} >
                 Contact
               </Link>
