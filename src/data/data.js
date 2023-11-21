b9deaa507441224f11782221a36d86349eb03b70
Oscar Santos <oscar303data@gmail.com>
2023-11-21 14:43:40 -0700

    add new project to my portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,5 @@
 import Chatbot from '../assets/projects/chatbot_2.png'
+import Travel from '../assets/projects/travel.png'
 import TaskTracker from '../assets/projects/task_tracker.png'
 import HistoryMaps from '../assets/projects/history_maps.png'
 import WeatherSweather from '../assets/projects/weather_sweather.png'
@@ -8,46 +9,53 @@ import Tic from '../assets/projects/tic.png'
 export const data=[
     {
         id:1,
+        name:"Discover Travel App",
+        image:Travel,
+        github:"https://github.com/Oscar-Santos/discover-travel-app",
+        live:"https://discover-travel-app.netlify.app/",
+    },
+    {
+        id:2,
         name:"OpenAI Chatbot App",
         image:Chatbot,
         github:"https://github.com/Oscar-Santos/OpenAI_ChatBot/blob/main/README.md",
         live:"https://open-ai-chat-bot-green.vercel.app/",
     },
     {
-        id:2,
-        name:"Ana Rio's Hair Salon Site",
-        image:Ana,
-        github:"https://github.com/Oscar-Santos/hair-salon",
-        live:"https://hair-salon-green.vercel.app/",
-    },
-    {
         id:3,
-        name:"Task Tracker App",
-        image:TaskTracker,
-        github:"https://github.com/Oscar-Santos/TASK_TRACKER_App",
-        live:"https://task-tracker-app-five.vercel.app/",
-    },
-    {
-        id:4,
-        name:"Tic Tac Toe Game",
-        image:Tic,
-        github:"https://github.com/Oscar-Santos/tic_tac_toe_game_javascript",
-        live:"https://oscar-santos.github.io/tic_tac_toe_game_javascript/",
-    },
-    {
-        id:5,
         name:"History Maps App",
         image:HistoryMaps,
         github:"https://github.com/History-Maps-2203",
         live:"",
     },
     {
-        id:6,
-        name:"Weather Sweather App",
-        image:WeatherSweather,
-        github:"https://github.com/Oscar-Santos/weather_sweater",
-        live:"",
+        id:4,
+        name:"Ana Rio's Hair Salon Site",
+        image:Ana,
+        github:"https://github.com/Oscar-Santos/hair-salon",
+        live:"https://hair-salon-green.vercel.app/",
     },
+    {
+        id:5,
+        name:"Task Tracker App",
+        image:TaskTracker,
+        github:"https://github.com/Oscar-Santos/TASK_TRACKER_App",
+        live:"https://task-tracker-app-five.vercel.app/",
+    },
+    {
+        id:6,
+        name:"Tic Tac Toe Game",
+        image:Tic,
+        github:"https://github.com/Oscar-Santos/tic_tac_toe_game_javascript",
+        live:"https://oscar-santos.github.io/tic_tac_toe_game_javascript/",
+    },
+    // {
+    //     id:6,
+    //     name:"Weather Sweather App",
+    //     image:WeatherSweather,
+    //     github:"https://github.com/Oscar-Santos/weather_sweater",
+    //     live:"",
+    // },
 
 
 ]
